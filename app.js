0e4df6a64d03c48003d9e464e261304ffe3ffd05
Daniyal ABBASI <153856061+Daniyal12222@users.noreply.github.com>
2024-08-17 11:05:12 +0500

    blog

---
+++ app.js
@@ -0,0 +1,32 @@
+//  /////////////////////////////////////////////////////////////////////////////////////////////////
+// // ======================================= fire base ========================================== //
+//  /////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+ 
+//  // Import the functions you need from the SDKs you need
+//  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+//  import { getAuth , createUserWithEmailAndPassword , signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+//  import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+//  // TODO: Add SDKs for Firebase products that you want to use
+//  // https://firebase.google.com/docs/web/setup#available-libraries
+ 
+//  // Your web app's Firebase configuration
+//  const firebaseConfig = {
+//    apiKey: "AIzaSyBZAI02pI8CUWyP8YEjEjNv1nXlb8VZ6bc",
+//    authDomain: "test-43a6f.firebaseapp.com",
+//    projectId: "test-43a6f",
+//    storageBucket: "test-43a6f.appspot.com",
+//    messagingSenderId: "922853907642",
+//    appId: "1:922853907642:web:b32069debd5c61da76b683"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const db=getFirestore(app);
+
+
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+// // ======================================= creat account ===================================== //
+// /////////////////////////////////////////////////////////////////////////////////////////////////
